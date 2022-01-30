4678018efe7a77783f6d4bd40cfe1ff68ee41cc4
Dayls <daniil.shmel@list.ru>
2022-01-30 15:18:42 +0400

    Add projects content and github button

--- scripts/align_mobile.js
+++ scripts/align_mobile.js
@@ -5,26 +5,49 @@ const project = document.getElementsByClassName("project")
 
 function align_website_small_screen() {
     
-    if(window.innerWidth < 955) {
-        b.style.marginLeft = 0;
-        b.style.marginRight = 0;
-    } else {
-        b.style.marginLeft = '17%';
-        b.style.marginRight = '17%';
+    if(window.innerWidth < 700) { 
+        applyMobileAlign();
+    } else if(window.innerWidth >= 700) {
+        applyNormalAlign();
     }
-
-    // check if the page runs on mobile device
-    if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        about_me.style.paddingLeft = '3px';
-        about_me.style.paddingRight = '3px';
-
-        projects.style.display = 'block';
-        for( i = 0; i < project.length; i++) {
-            project[i].style.marginTop = '1em';
-        }
+    
+    if(window.innerWidth < 955) {
+        applySmallWindow();
+    } else {
+        applyNormalWindow();
     }
 
     setTimeout('align_website_small_screen()', 1);
+}
+
+function applyMobileAlign() {
+    about_me.style.paddingLeft = '3px';
+    about_me.style.paddingRight = '3px';
+
+    projects.style.display = 'block';
+    for( i = 0; i < project.length; i++) {
+        project[i].style.marginTop = '1em';
+    }
+}
+
+function applyNormalAlign() {
+    about_me.style.paddingLeft = '100px';
+    about_me.style.paddingRight = '100px';
+
+    projects.style.display = 'grid';
+    for( i = 0; i < project.length; i++) {
+        project[i].style.marginTop = '0';
+    }
+}
+
+function applySmallWindow() {
+    b.style.marginLeft = 0;
+    b.style.marginRight = 0;
+}
+
+function applyNormalWindow() {
+    b.style.marginLeft = '17%';
+    b.style.marginRight = '17%';
 }
 
 
